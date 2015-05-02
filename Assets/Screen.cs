dacdd09e7b0cd081e307b118d6e097e68bad595d
Raziaar <raziaar@gmail.com>
2015-05-01 22:27:50 -0500

    Raycast testing

--- Assets/Screen.cs
+++ Assets/Screen.cs
@@ -5,11 +5,28 @@ public class Screen : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
+
+    void OnMouseDown()
+    {
+        //Debug.Log("Mouse Clicked");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void PlaceObjectRaycast(GameObject theObject)
+    {
+        Ray worldPosition = Camera.main.ViewportPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(worldPosition, out hit))
+        {
+            Instantiate(theObject, hit.point, Quaternion.identity);
+            Debug.Log(theObject.name + " Created");
+        }
+    }
 }
