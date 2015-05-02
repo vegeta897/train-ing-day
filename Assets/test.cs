ec4a42afdf55425f2880fa56f5621d20dc9c9c90
vegeta897 <vegeta897@gmail.com>
2015-05-01 21:45:59 -0400

    2D array map test

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class test : MonoBehaviour {
+public class test : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
-	    
+	void Start () 
+    {
+        int[,] map = new int[100, 100];
+        map[50, 50] = 3;
+        Debug.Log(map[50,50]);
 	}
-	
-    // Anything <- Key to traindrain. traindraino... powers through the toughest drain grime.
 
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	    
 	}
 }
